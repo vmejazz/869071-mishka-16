80e0e8cb1bb3af5abf5365b7f6465d9ed18f294c
vmejazz <vmejazz@gmail.com>
2019-04-19 12:09:27 +0300

    Правка по коммитам. Домашка 4

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,10 +1,10 @@
 var menuToggle = document.querySelector(".page-header__menu-toggle");
 var menuList = document.querySelector(".page-header__menu-list");
-var shopperList = document.querySelector(".page-header__shopper-list");
+var shopperMenu = document.querySelector(".page-header__shopper-menu");
 
 menuToggle.addEventListener("click", function(evt) {
   evt.preventDefault ();
   menuList.classList.toggle("show-menu");
-  shopperList.classList.toggle("show-menu");
+  shopperMenu.classList.toggle("show-menu");
   menuToggle.classList.toggle("page-header__menu-toggle--open");
 });
